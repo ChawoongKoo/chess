3ff8ef146623b0d07bc148c8d0cc5d36c7ceed37
chawoong <chawoong@umich.edu>
2025-07-04 21:24:25 -0400

    added basic chess game functionality

--- MainChess.py
+++ MainChess.py
@@ -1,5 +1,27 @@
 import chess
+import os
 
 board = chess.Board()
 
-print(board)
+while True:
+    os.system("clear")
+    print(board)
+    user_input = input("Enter a move (or 'q' to quit): ")
+    os.system("clear")
+
+    if user_input.lower() == 'q':
+        print("Exiting.")
+        break
+
+    print(f"You entered: {user_input}")
+    try:
+        move = chess.Move.from_uci(user_input)
+        board.push(move)
+        print(board)
+
+        if(board.is_checkmate()):
+            print("Checkmate!")
+            break
+    except:
+        print("Please enter a move in UCI format.")
+
