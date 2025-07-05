3bf18cdc13c33962ef70cf9647a722cdf44c6a3c
chawoong <chawoong@umich.edu>
2025-07-05 12:58:03 -0400

    added messages to make clear which side to play

--- MainChess.py
+++ MainChess.py
@@ -7,16 +7,16 @@ board = chess.Board()
 # print(board.legal_moves)
 # def random_move() -> chess.Move:
 
-
 while True:
     os.system("clear")
     print(board)
+    print(f"\nWhite to move." if board.turn else "Black to move")
     user_input = input("Enter a move (or 'q' to quit): ")
     os.system("clear")
 
     if user_input.lower() == 'q':
         print("Exiting.")
-        time.sleep(1)
+        time.sleep(.5)
         os.system("clear")
         break
 
@@ -25,8 +25,10 @@ while True:
         move = board.push_san(user_input)
         print(board)
 
-        if(board.is_checkmate()):
+        outcome = board.outcome()
+        if(outcome):
             print("Checkmate!")
+            print(outcome.result())
             break
     except:
         print("Please enter a move in SAN format.")
