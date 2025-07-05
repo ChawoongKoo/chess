95a4134b260a2d8b0ca2ac748da0d5ccbfe14378
chawoong <chawoong@umich.edu>
2025-07-05 12:33:17 -0400

    updated logic to use SAN instead of UCI, added checkmate statement

--- MainChess.py
+++ MainChess.py
@@ -1,7 +1,12 @@
 import chess
 import os
+import time
 
 board = chess.Board()
+
+# print(board.legal_moves)
+# def random_move() -> chess.Move:
+
 
 while True:
     os.system("clear")
@@ -11,17 +16,18 @@ while True:
 
     if user_input.lower() == 'q':
         print("Exiting.")
+        time.sleep(1)
+        os.system("clear")
         break
 
     print(f"You entered: {user_input}")
     try:
-        move = chess.Move.from_uci(user_input)
-        board.push(move)
+        move = board.push_san(user_input)
         print(board)
 
         if(board.is_checkmate()):
             print("Checkmate!")
             break
     except:
-        print("Please enter a move in UCI format.")
-
+        print("Please enter a move in SAN format.")
+        time.sleep(1.5)
