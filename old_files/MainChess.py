df2df51cdce9d4f390814151c3c2d63b989b027b
chawoong <chawoong@umich.edu>
2025-07-08 11:57:19 -0400

    moved old files into their own folder

---
+++ old_files/MainChess.py
@@ -0,0 +1,42 @@
+import chess
+import os
+import time
+import random
+
+board = chess.Board()
+
+print(board.legal_moves)
+def random_move() -> chess.Move:
+    return random.choice(list(board.legal_moves))
+
+while True:
+    os.system("clear")
+    print(board)
+    if not board.turn:
+        move = random_move()
+        board.push(move)
+        time.sleep(.5)
+        continue
+    print(f"\nWhite to move." if board.turn else "Black to move")
+    user_input = input("Enter a move (or 'q' to quit): ")
+    os.system("clear")
+
+    if user_input.lower() == 'q':
+        print("Exiting.")
+        time.sleep(.5)
+        os.system("clear")
+        break
+
+    print(f"You entered: {user_input}")
+    try:
+        move = board.push_san(user_input)
+        print(board)
+
+        outcome = board.outcome()
+        if(outcome):
+            print("Checkmate!")
+            print(outcome.result())
+            break
+    except:
+        print("Please enter a move in SAN format.")
+        time.sleep(1.5)
---
+++ old_files/README.md
@@ -0,0 +1,4 @@
+# chess
+I made this chess ai bot during the summer after my college graduation. 
+I was really bored and wanted something fun to do that could distract me from other work.
+I hope I end up finishing it.
