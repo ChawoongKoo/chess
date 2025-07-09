68b95b8d79f4c0376e1e40f1eb5372f1efcd254f
chawoong <chawoong@umich.edu>
2025-07-09 11:14:09 -0400

    added evaluation function, minimax test skeleton

--- engines/minimax.py
+++ engines/minimax.py
@@ -1,12 +1,21 @@
 import chess
-from chess.engine import PlayResult, Limit
+from chess.engine import PlayResult
 import random
 from lib.engine_wrapper import MinimalEngine
 from lib.lichess_types import MOVE, HOMEMADE_ARGS_TYPE
-import logging
+from piece_value import value
 
 class MiniMax(MinimalEngine):
     """Minimax algorithm"""
 
     def search(self, board: chess.Board, *args: HOMEMADE_ARGS_TYPE) -> PlayResult:
         return PlayResult(move=random.choice(list(board.legal_moves)), ponder=None)
+    
+    def evaluate(self, board: chess.Board) -> float:
+        score = 0
+        for piece in board.piece_map().values():
+            if board.turn:
+                score += value(piece.piece_type)
+            else:
+                score -= value(piece.piece_type)
+        return score
